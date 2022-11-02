826272e74c6309de86e7de3a244dd29f8340400c
Hasnainqureshi1 <husnainqureshi134@gmail.com>
2022-11-02 08:04:34 -0700

    dashboard html css

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+ //menu toggle
+//  const toggle = document.querySelector('.toggle');
+//  const navigation = document.querySelector('.navigation');
+//  const main = document.querySelector('.main');
+//  toggle.onclick = function(){
+    //  navigation.classList.toggle('active');
+    // console.log('toggle');     
+//  }
